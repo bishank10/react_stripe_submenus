7df00065820d363fd4d6d93570e9073aad1bbf26
bishank <chandbishank@gmail.com>
2020-12-23 11:31:29 -0700

    global context for modal and side bar defined and incorporated in app

--- src/context.js
+++ src/context.js
@@ -1,2 +1,36 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
+    return <AppContext.provide value={{
+        isModalOpen,
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal
+    }}>
+        {children}
+    </AppContext.provide>
+}
